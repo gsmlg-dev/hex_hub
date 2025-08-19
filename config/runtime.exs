3554b4849d824399fd24dc8ea422fe1812605794
Jonathan Gao <gsmlg.com@gmail.com>
2025-08-19 16:41:11 +0800

    fix: Fix.

--- config/runtime.exs
+++ config/runtime.exs
@@ -8,7 +8,7 @@ if config_env() == :prod do
       You can generate one by calling: mix phx.gen.secret
       """
 
-  host = System.get_env("PHX_HOST") || "example.com"
+  host = System.get_env("PHX_HOST") || "hex-hub.dev"
   port = String.to_integer(System.get_env("PORT") || "4000")
 
   config :hex_hub, :dns_cluster_query, System.get_env("DNS_CLUSTER_QUERY")
@@ -20,4 +20,19 @@ if config_env() == :prod do
       port: port
     ],
     secret_key_base: secret_key_base
+
+  host = System.get_env("ADMIN_PHX_HOST") || "admin.hex-hub.dev"
+  port = String.to_integer(System.get_env("ADMIN_PORT") || "4001")
+
+  config :hex_hub, HexHubAdminWeb.Endpoint,
+    url: [host: host, port: 443, scheme: "https"],
+    http: [
+      ip: {0, 0, 0, 0, 0, 0, 0, 0},
+      port: port
+    ],
+    secret_key_base: secret_key_base
+
+  config :hex_hub,
+    storage_type: :local,
+    storage_path: System.get_env("STORAGE_PATH", "priv/storage")
 end
